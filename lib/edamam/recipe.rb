b03e16e95f0ab654b98fa61e49f5fb2eb3108819
Kira Helm <soulclimberchick@gmail.com>
2020-01-17 11:45:43 -0800

    added nutrient search ability

--- lib/edamam/recipe.rb
+++ lib/edamam/recipe.rb
@@ -3,9 +3,12 @@ require "httparty"
 module Edamam
   class Recipe
     def self.search(q)
+      params = q.split(" ").select { |param| param.match("nutrients") }
+      param_string = params.join("&")
+      q = (q.split(" ") - params).join(" ")
       app_id = ENV.fetch("APP_ID")
       app_key = ENV.fetch("APP_KEY")
-      url = "https://api.edamam.com/search?q=#{q}&app_id=#{app_id}&app_key=#{URI.encode(app_key)}"
+      url = "https://api.edamam.com/search?q=#{q}&app_id=#{app_id}&app_key=#{URI.encode(app_key)}&#{param_string}"
       results = JSON.parse(HTTParty.get(url).body)
       results["hits"]
     end
